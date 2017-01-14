9764035cd2e4b2119a5f3c20b7ff600b6bd2fb60
Aleksander Stefanski <Aleksander Stefanski>
2017-01-14 13:19:20 +0100

    Add context to test

---
+++ src/test/java/com/crimson/context/TestSpringCore.java
@@ -0,0 +1,34 @@
+package com.crimson.context;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.crimson")
+@Import({SpringCore.class})
+public class TestSpringCore {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUsername("root");
+        dataSource.setPassword("admin");
+        return dataSource;
+    }
+
+
+
+}
+
