ccb8e9969661d15e63e1db74ea7379cbebca8449
sherrywolf <daria.wieliczko@wp.pl>
2017-02-27 21:26:56 +0100

    added simple validation in model Comment

--- src/main/java/com/crimson/core/model/Comment.java
+++ src/main/java/com/crimson/core/model/Comment.java
@@ -3,8 +3,10 @@ package com.crimson.core.model;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -18,9 +20,12 @@ public @Data class Comment {
     private Long id;
 
     @Column(name = "commentText")
+    @Length(min = 10,max = 500)
+    @NotNull
     private String text;
 
     @Column(name = "commentDate")
+    @NotNull
     private LocalDate date;
 
     @Version
